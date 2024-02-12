f7b9d105e0f05d0e6a9f7c56dc76a9ad14d290f1
masa-gradstudent <ganeisyatotomoni@icloud.com>
2024-02-12 10:44:51 +0900

    #14 modelの作成

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# 投稿記事（Article）
+class Article(models.Model):
+    title = models.CharField(max_length = 255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.title # 記事タイトルを返す
+
+
