29f1170a07bdeedacd45eb925971ff4f45e144ac
masa-gradstudent <ganeisyatotomoni@icloud.com>
2024-02-12 11:08:35 +0900

    #14 views作成

--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
+from .models import Article
 # Create your views here.
 
 def home_view(request):
     return render(request, "home/home.html")
 
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
 
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html '   
