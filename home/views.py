74bfc7e3b3343f2bd39da22008011ad5b1e4761e
masa-gradstudent <ganeisyatotomoni@icloud.com>
2024-02-12 20:34:41 +0900

    #16 views.py作成

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Article
-# Create your views here.
+from .forms import CommentForm
+
 
 def home_view(request):
     return render(request, "home/home.html")
@@ -15,7 +16,18 @@ class ArticleListView(ListView):
 
 class ArticleDetailView(DetailView):
     model = Article
-
     template_name = 'home/article_detail.html'   
     #コンテキスト名を設定
     context_object_name = 'article'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #投稿されたコメントの全権を取得
+        comments = self.object.comments.all()
+        #コメントフォームを設置
+        form = CommentForm()
+        context.update({
+            'comments': comments, 
+            'form': form,
+        })
+        return context
