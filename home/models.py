8904d0ee3af7ce9a0986d50104c10d85d6d20041
masa-gradstudent <ganeisyatotomoni@icloud.com>
2024-02-12 20:06:15 +0900

    ＃16 commentモデル作成

--- home/models.py
+++ home/models.py
@@ -10,4 +10,11 @@ class Article(models.Model):
     def __str__(self):
         return self.title # 記事タイトルを返す
 
-
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length = 50, blank = True, default = '匿名')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    approved_comment = models.BooleanField(default = False)
+    def __str__(self):
+        return self.text 
