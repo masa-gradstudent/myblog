4995926762f905755f7ac02e07559eda1f5030d1
masa-gradstudent <ganeisyatotomoni@icloud.com>
2024-02-19 23:36:54 +0900

    #27 urlの修正

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import home_view, ArticleListView, ArticleDetailView
 
 urlpatterns = [
-    path('', home_view, name='home'),
-    path('list/', ArticleListView.as_view(), name='article_list'),
+    # path('', home_view, name='home'),
+    path('', ArticleListView.as_view(), name='article_list'),
     path('article/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
 ]
